49da5b708d5f2eed1581531098261e510be295ac
dejetem <enyeoluwarotimisimon@gmail.com>
2023-02-09 16:32:33 +0100

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,7 @@
 -- use attributes formed and split for computing the lifespan
 
 
-SELECT DISTINCT band_name, IFNULL(`split`, 2020) - `formed` as lifespan
-FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)ORDER BY lifespanDESC;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
