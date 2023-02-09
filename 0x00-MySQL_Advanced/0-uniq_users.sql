e8a88589034ee20479cb3e9d696cb682d7bff70b
dejetem <enyeoluwarotimisimon@gmail.com>
2023-02-09 15:27:02 +0100

    task

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- Creates a table with unique users.
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
